8892ac5f662c9e668ce23c1dfcac2ec2c7779a23
nOmeep <37767676+nOmeep@users.noreply.github.com>
2022-04-23 21:46:58 +0300

    add-main-activity

--- app/src/main/java/com/example/cryptocurrencyapplication/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyapplication/MainActivity.kt
@@ -5,8 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptocurrencyapplication.presentation.Screen
+import com.example.cryptocurrencyapplication.presentation.coin_detail.components.CoinDetailScreen
+import com.example.cryptocurrencyapplication.presentation.coin_list.components.CoinListScreen
 import com.example.cryptocurrencyapplication.presentation.ui.theme.CryptocurrencyApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +22,22 @@ class MainActivity : ComponentActivity() {
             CryptocurrencyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
